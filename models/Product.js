a8f194c6de1c0ec3e86e58d3be4224c161b0eae4
Fernandojmo <fernando.javier.melo@gmail.com>
2023-05-04 17:41:05 -0400

    arreglos

--- models/Product.js
+++ models/Product.js
@@ -62,36 +62,3 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model("product", productSchema)
 
 module.exports = Product;
-// const mongoose=require('mongoose');
-// const productSchema = new mongoose.Schema({
-//     sku: {
-//         type: String,
-//         required: true
-//     },
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     price: {
-//         type: Number,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: true
-//     },
-//     image: {
-//         type: String,
-//         required: true
-//     },
-//     category: {
-//         type: String,
-//         required: true
-//     },
-//     stock: {
-//         type: Number,
-//         required: true
-//     }
-// })
-// const Product = mongoose.model('product', productSchema);
-// module.exports = Product;
